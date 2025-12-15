828a7df5e1701c1e02936b33ce8ee9d4b9e30c8b
popomomlolo <popomomlolo@gmail.com>
2025-12-15 11:02:18 +0100

    2eme commit

--- PopSauce_Serveur/widgetpopsauceserveur.cpp
+++ PopSauce_Serveur/widgetpopsauceserveur.cpp
@@ -6,10 +6,42 @@ WidgetPopSauceServeur::WidgetPopSauceServeur(QWidget *parent)
     , ui(new Ui::WidgetPopSauceServeur)
 {
     ui->setupUi(this);
+    connect(&sockEcoute, &QTcpServer::newConnection,
+            this, &WidgetPopSauceServeur::onQTcpServer_newConnection);
 }
 
 WidgetPopSauceServeur::~WidgetPopSauceServeur()
 {
     delete ui;
+}
+
+void WidgetPopSauceServeur::on_pushButtonLancer_clicked()
+{
+    sockEcoute.listen(QHostAddress::Any, ui->spinBoxPort->value());
+    qDebug() << "Serveur lancé";
+}
+
+void WidgetPopSauceServeur::onQTcpServer_newConnection()
+{
+    QTcpSocket *client = sockEcoute.nextPendingConnection();
+    connect(client, &QTcpSocket::readyRead,
+            this, &WidgetPopSauceServeur::onQTcpSocket_readyRead);
+    connect(client, &QTcpSocket::disconnected,
+            this, &WidgetPopSauceServeur::onQTcpSocket_disconnected);
+    listeDesClients.append(client);
+    qDebug() << "Nouvelle connexion de" << client->peerAddress().toString();
+
+}
+
+void WidgetPopSauceServeur::onQTcpSocket_disconnected()
+{
+    QTcpSocket *client = qobject_cast<QTcpSocket*>(sender());
+    qDebug() << "Déconnexion de" << client->peerAddress().toString();
+    listeDesClients.removeOne(client);
+}
+
+void WidgetPopSauceServeur::onQTcpSocket_readyRead()
+{
+
 }
 
--- PopSauce_Serveur/widgetpopsauceserveur.h
+++ PopSauce_Serveur/widgetpopsauceserveur.h
@@ -2,6 +2,9 @@
 #define WIDGETPOPSAUCESERVEUR_H
 
 #include <QWidget>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QBuffer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class WidgetPopSauceServeur; }
@@ -14,8 +17,14 @@ class WidgetPopSauceServeur : public QWidget
 public:
     WidgetPopSauceServeur(QWidget *parent = nullptr);
     ~WidgetPopSauceServeur();
-
+private slots:
+    void on_pushButtonLancer_clicked();
+    void onQTcpServer_newConnection();
+    void onQTcpSocket_disconnected();
+    void onQTcpSocket_readyRead();
 private:
     Ui::WidgetPopSauceServeur *ui;
+    QTcpServer sockEcoute;
+    QList<QTcpSocket*> listeDesClients;
 };
 #endif // WIDGETPOPSAUCESERVEUR_H
