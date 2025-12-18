abec1d10cf773e8b0d3449f0622452db431625d1
RandomBug14 <randombug155@gmail.com>
2025-12-18 08:14:39 +0100

    Update moc_widgetpopsauceserveur.cpp
    tkt

--- PopSauce_Serveur/moc_widgetpopsauceserveur.cpp
+++ PopSauce_Serveur/moc_widgetpopsauceserveur.cpp
@@ -119,22 +119,5 @@ int WidgetPopSauceServeur::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     return _id;
 }
 
-void WidgetPopSauceServeur::recupererQuestionIndice()
-{
-QSqlQuery requetePrepare;
-requetePrepare.prepare("SELECT question, indice FROM "question" WHERE id_quest = :id;");
 
-//requetePrepare.bindValue(":id",nbRandom);
-if (!requetePrepare.exec()){
-    qDebug()<<"pb requete "<<requetePrepare.lastError();
-}
-
-QString question, indice;
-while(requetePrepare.next())
-{
-    question=requetePrepare.value("question").toString();
-    indice=requetePrepare.value("indice").toString();
-    qDebug()<<question << indice;
-}
-}
 QT_WARNING_POP
